be4bf49af75061bf4f4349ada46659cb52e51493
Weijia Wang <starkewang@tencent.com>
2022-01-11 11:46:21 +0800

    代码格式化

--- index.js
+++ index.js
@@ -1,62 +1,61 @@
-const path = require('path')
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-const { init: initDB, Counter } = require('./db')
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { init: initDB, Counter } = require("./db");
 
-const logger = morgan('tiny')
+const logger = morgan("tiny");
 
-const app = express()
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(cors())
-app.use(logger)
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(logger);
 
 // 首页
-app.get('/', async (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
-})
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 // 更新计数
-app.post('/api/count', async (req, res) => {
-  const { action } = req.body
-  if (action === 'inc') {
-    await Counter.create()
-  } else if (action === 'clear') {
+app.post("/api/count", async (req, res) => {
+  const { action } = req.body;
+  if (action === "inc") {
+    await Counter.create();
+  } else if (action === "clear") {
     await Counter.destroy({
-      truncate: true
-    })
+      truncate: true,
+    });
   }
   res.send({
     code: 0,
-    data: await Counter.count()
-  })
-})
+    data: await Counter.count(),
+  });
+});
 
 // 获取计数
-app.get('/api/count', async (req, res) => {
-  const result = await Counter.count()
+app.get("/api/count", async (req, res) => {
+  const result = await Counter.count();
   res.send({
     code: 0,
-    data: result
-  })
-})
+    data: result,
+  });
+});
 
 // 小程序调用，获取微信 Open ID
-app.get('/api/wx_openid', async (req, res) => {
-  if (req.headers['x-wx-source']) {
-    res.send(req.headers['x-wx-openid'])
+app.get("/api/wx_openid", async (req, res) => {
+  if (req.headers["x-wx-source"]) {
+    res.send(req.headers["x-wx-openid"]);
   }
-})
+});
 
-const port = process.env.PORT || 80
+const port = process.env.PORT || 80;
 
 async function bootstrap() {
-  await initDB()
+  await initDB();
   app.listen(port, () => {
-    console.log('启动成功', port)
-  })
+    console.log("启动成功", port);
+  });
 }
 
 bootstrap();
-
