3f5a3cafe0d01c0f9d86b1d1fc7cd0ae9bc6204a
xinxinhe <hexin_33@126.com>
2022-08-18 20:47:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,38 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
+function sendmess (mess) {
+  const appid = 'wx3a426761d23d42b3'
+  return new Promise((resolve, reject) => {
+    request({
+      method: 'POST',
+      url: `http://api.weixin.qq.com/cgi-bin/message/custom/send?from_appid=${appid}`,
+      body: JSON.stringify(mess)
+    }, function (error, response) {
+      if (error) {
+        console.log('接口返回错误', error)
+        reject(error.toString())
+      } else {
+        console.log('接口返回内容', response.body)
+        resolve(response.body)
+      }
+    })
+  })
+}
+
 // 首页
 app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+  sendmess({
+    touser: FromUserName,
+    msgtype: 'link',
+    link: {
+      title: 'Relax｜今日推荐音乐',
+      description: '每日推荐一个好听的音乐，感谢收听～',
+      thumb_url: 'https://y.qq.com/music/photo_new/T002R300x300M000004NEn9X0y2W3u_1.jpg?max_age=2592000', // 支持JPG、PNG格式，较好的效果为大图360*200，小图200*200
+      url: 'https://c.y.qq.com/base/fcgi-bin/u?__=0zVuus4U'
+    }
+  })
 });
 
 // 更新计数
